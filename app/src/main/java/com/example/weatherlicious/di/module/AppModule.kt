a49072b4279f68941224c1d7d063b9557071f8fd
agul1no <augusto.cera@gmail.com>
2022-07-13 20:16:55 +0200

    Dependency injection implemented Application class created Activity and Fragments annotated App Module created and provideRetrofit function created Util package created

---
+++ app/src/main/java/com/example/weatherlicious/di/module/AppModule.kt
@@ -0,0 +1,22 @@
+package com.example.weatherlicious.di.module
+
+import com.example.weatherlicious.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
